9cb27c4ff97e74546c889e450aad3ff181b6a8d5
miyass <exe.miyakura0629@gmail.com>
2019-02-11 16:36:53 +0900

    initial commit

---
+++ firebaseDevEnv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseDevEnv
+//
+//  Created by 宮倉宗平 on 2019/02/11.
+//  Copyright © 2019 Sohei Miyakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
